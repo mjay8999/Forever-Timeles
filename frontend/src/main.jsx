3e027e46cee1e6ed244d6ed367a0d1ae7af1dc58
Jay <mjay8999@gmail.com>
2025-12-10 12:57:25 +0530

    Fix GitHub Pages routing: use HashRouter and add 404.html

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 )
